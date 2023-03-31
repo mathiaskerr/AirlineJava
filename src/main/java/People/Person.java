98c568c879be2ab86ab46691e5ce8b55d95239ff
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-03-31 10:48:28 +0100

    fist commit

---
+++ src/main/java/People/Person.java
@@ -0,0 +1,29 @@
+package People;
+
+public abstract class Person {
+
+    private String name;
+
+    private Integer weight;
+
+    public Person (String name, Integer weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+}
